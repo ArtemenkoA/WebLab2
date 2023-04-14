48e57fe0f64ef48e1eb1bafedecaf48608c315a8
79152 <7915241@gmail.com>
2023-04-14 16:50:05 +0300

    /

---
+++ WebLab2/Controllers/Class.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebLab2.Controllers
+{
+    public class Detail
+    {
+        public int Id { get; set; }
+        public int Count { get; set; }
+        public int Adres { get; set; }
+    }
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Detail> Details => Set<Detail>();
+        public ApplicationContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+}
